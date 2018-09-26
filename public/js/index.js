de062f0ed24c45f77b2274f1d0731eaed28d2f87
Divyanshu Rathore <divyanshu03@live.com>
2018-09-26 16:02:05 +0530

    Adds location broadcast

--- public/js/index.js
+++ public/js/index.js
@@ -9,7 +9,6 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(message) {
-  console.log('New message', message);
   var li = $('<li></li>');
   li.text(`${message.from}: ${message.text}`);
 
@@ -23,6 +22,16 @@ socket.emit('createMessage', {
   console.log('Message received', data);
 });
 
+socket.on('newLocationMessage', function(message) {
+  var li = $('<li></li>');
+  var a = $('<a target="_blank">My current Location</a>');
+  
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  $('#messages').append(li);
+});
+
 $('#message-form').on('submit', function(e) {
   e.preventDefault();
 
@@ -32,4 +41,21 @@ $('#message-form').on('submit', function(e) {
   }, function(data) {
     
   });
+});
+
+var locationButton = $('#send-location');
+
+locationButton.on('click', function() {
+  if(!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser');
+  }
+
+  navigator.geolocation.getCurrentPosition(function(position){
+    socket.emit('createLocationMessage', {
+      lat: position.coords.latitude,
+      long: position.coords.longitude
+    })
+  }, function(e) {
+    alert('Unable to fetch location.');
+  })
 });
