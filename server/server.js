f322749591d57d0a8866765f29ca4af3bf1389e1
Divyanshu Rathore <divyanshu03@live.com>
2018-09-26 14:31:11 +0530

    Listens and responds to new message event from create message

--- server/server.js
+++ server/server.js
@@ -1,12 +1,32 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.emit('createMessage', (message) => {
+    io.emit('newMessage', {
+      form: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+}); 
+
+server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
