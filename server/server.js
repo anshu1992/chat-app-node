0ebb648afd01cb1dfd76b5c170c55cb6caee4276
Divyanshu Rathore <divyanshu03@live.com>
2018-09-26 14:42:49 +0530

    Greets new user upon join

--- server/server.js
+++ server/server.js
@@ -14,12 +14,23 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('createMessage', (message) => {
-    io.emit('newMessage', {
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app'
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+  
+  socket.on('createMessage', (message) => {
+    socket.broadcast.emit('newMessage', {
       form: message.from,
       text: message.text,
       createdAt: new Date().getTime()
-    });
+    })
   });
 
   socket.on('disconnect', () => {
